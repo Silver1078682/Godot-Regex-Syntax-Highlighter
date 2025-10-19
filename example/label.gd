b8137ae2384419684c0e8206fef6f0b18d71248d
Silver1078682 <ShinySmoothSilver@outlook.com>
2025-10-19 17:43:56 +0800

    feat: now the result label in demo will turn gray with an invalid regex

--- example/label.gd
+++ example/label.gd
@@ -5,21 +5,23 @@ extends Label
 
 var _regex:= RegEx.new()
 func _ready() -> void:
-	var test := RegEx.create_from_string(r"(?(DEFINE)(?<one>1))(?&one)")
 	_update()
 
 func _update():
 	_regex.compile(regex_edit.text, false)
+
 	if not _regex.is_valid():
+		modulate = Color.DIM_GRAY
 		text = "NOT VALID"
-		return
 		
-	text = ""
-	var result := _regex.search_all(text_edit.text)
-	for i in result:
-		if not i.get_string():
-			continue
-		else:
-			text += i.get_string()
-			text += "\n" if check_box.button_pressed else ""
-	text += "\b"
+	else:
+		modulate = Color.WHITE
+		text = ""
+		var result := _regex.search_all(text_edit.text)
+		for i in result:
+			if not i.get_string():
+				continue
+			else:
+				text += i.get_string()
+				text += "\n" if check_box.button_pressed else ""
+		text += "\b"
